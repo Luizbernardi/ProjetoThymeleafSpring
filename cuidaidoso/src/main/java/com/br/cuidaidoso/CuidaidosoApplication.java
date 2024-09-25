40cb5092133e03f163d8cb9e78da43dd56818147
Luizbernardi <luizbernardi37@gmail.com>
2024-09-25 10:59:18 -0300

    fix iniciando projeto

---
+++ cuidaidoso/src/main/java/com/br/cuidaidoso/CuidaidosoApplication.java
@@ -0,0 +1,13 @@
+package com.br.cuidaidoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidaidosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidaidosoApplication.class, args);
+	}
+
+}
---
+++ cuidaidoso/src/test/java/com/br/cuidaidoso/CuidaidosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cuidaidoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidaidosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
